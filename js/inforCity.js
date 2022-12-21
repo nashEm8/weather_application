0da872bb48478b5bc8af011fc862c3a1031edb89
nashEm8 <emanuel82022@outlook.com>
2022-12-21 07:11:02 -0300

    Criando requisição que vai pegar os dados do clima de uma cidade, por meio de uma chave key passada

--- js/inforCity.js
+++ js/inforCity.js
@@ -5,22 +5,42 @@ const apiKey = 'Krxf15YbYiQuEGuxxY3FIGZLWgLGoEnw';
 const getCityUrl = cityName => 
 `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
 
+
 const getCityData = async cityName => {
     try{
-        const cityUrl = getCityUrl(cityName);
+        const cityUrl = getCityUrl(cityName)
         const response = await fetch(cityUrl);
 
         if(!response.ok){
-           throw new Error('Não foi possível obter os dados!');
-        }
+         throw new Error('Não foi possível obter os dados!');
+     }
 
-        const [firstItemCityData] = await response.json();
+     const [firstItemCityData] = await response.json();
 
-        return firstItemCityData;
+     return firstItemCityData;
 
-    }   catch({name, message}){
-        console.log(`${name}: ${message}`)
-    }
+ }   catch({name, message}){
+    console.log(`${name}: ${message}`)
+}
 }
 
-getCityData('Cuiabá');
+const getCityClimate = async cityName => {
+    try{
+        const {Key} = await getCityData(cityName);
+        const cityWeatherUrl =`http://dataservice.accuweather.com/currentconditions/v1/${Key}?apiKey=${apiKey}`;
+        const response = await fetch(cityWeatherUrl);
+
+        if(!response.ok){
+         throw new Error('Não foi possível obter os dados!');
+     }
+
+     const [cityWeatherData] = await response.json();
+
+     return cityWeatherData;
+
+ }   catch({name, message}){
+    console.log(`${name}: ${message}`)
+}
+}
+
+getCityClimate('Fortaleza')
