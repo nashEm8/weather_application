ceecb3571cacc3ab267fa637dd225757aea4f2fc
nashEm8 <emanuel82022@outlook.com>
2022-12-22 23:10:44 -0300

    Refatorando inforCity.js

--- js/inforCity.js
+++ js/inforCity.js
@@ -2,44 +2,36 @@ const form = document.querySelector('form');
 const inputCity = document.querySelector('input')
 
 const apiKey = 'Krxf15YbYiQuEGuxxY3FIGZLWgLGoEnw';
+const baseUrl = 'http://dataservice.accuweather.com/';
+
 const getCityUrl = cityName => 
-`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
+`${baseUrl}locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
+
+const getWeatherUrl = ({ Key }) => 
+`${baseUrl}currentconditions/v1/${Key}?apikey=${apiKey}&language=pt-br`;
 
 
-const getCityData = async cityName => {
+const fetchData = async url => {
     try{
-        const cityUrl = getCityUrl(cityName)
-        const response = await fetch(cityUrl);
+        const response = await fetch(url);
 
         if(!response.ok){
          throw new Error('Não foi possível obter os dados!');
      }
 
-     const [firstItemCityData] = await response.json();
-
-     return firstItemCityData;
+     return response.json();
 
  }   catch({name, message}){
     console.log(`${name}: ${message}`)
-}
-}
-
-const getCityClimate = async cityName => {
-    try{
-        const {Key} = await getCityData(cityName);
-        const cityWeatherUrl =`http://dataservice.accuweather.com/currentconditions/v1/${Key}?apiKey=${apiKey}&language=pt-br`;
-        const response = await fetch(cityWeatherUrl);
-
-        if(!response.ok){
-         throw new Error('Não foi possível obter os dados!');
-     }
-
-     const [cityWeatherData] = await response.json();
-     return cityWeatherData;
-
- }   catch({name, message}){
-    console.log(`${name}: ${message}`)
-}
+    }
 }
 
-getCityClimate('Fortaleza')
+const getCityData = cityName => fetchData(getCityUrl(cityName))
+
+const getCityWeather = async cityName => {
+    const [cityData] = await getCityData(cityName);
+    return fetchData(getWeatherUrl(cityData));
+} 
+
+getCityWeather('São Paulo')
+    .then(console.log)
