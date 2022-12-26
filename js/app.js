437786fb8291d4e724e563c69cb5860a37c21194
nashEm8 <emanuel82022@outlook.com>
2022-12-26 08:30:31 -0300

    Removendo card e exibindo apenas quando o usuário digita o nome da cidade e envia

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ const cityForm = document.querySelector('[data-js="change-location"]');
 const showCityName = document.querySelector('.my-3');
 const showClimate = document.querySelector('div.my-3');
 const showTemperature = document.querySelector('.my-4 span');
+const cityCard = document.querySelector('[data-js=city-card]');
 
 cityForm.addEventListener('submit', async event => {
 	event.preventDefault();
 	const inputValue = event.target.city.value;
 	const [{ Key, LocalizedName }] = await getCityData(inputValue);
 	const [{ Temperature, WeatherText, }] = await getCityWeather(Key);
+
+	if(cityCard.classList.contains('d-none')){
+		cityCard.classList.remove('d-none');
+	}
 
 	showCityName.innerHTML = LocalizedName;
 	showClimate.innerHTML = WeatherText;
