95d020522c4cdd31691d29ad4ba66391e6b531ab
nashEm8 <emanuel82022@outlook.com>
2022-12-25 21:29:19 -0300

    Refatorando código

--- js/inforCity.js
+++ js/inforCity.js
@@ -7,8 +7,8 @@ const baseUrl = 'http://dataservice.accuweather.com/';
 const getCityUrl = cityName => 
 `${baseUrl}locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
 
-const getWeatherUrl = ({ Key }) => 
-`${baseUrl}currentconditions/v1/${Key}?apikey=${apiKey}&language=pt-br`;
+const getWeatherUrl = cityKey => 
+`${baseUrl}currentconditions/v1/${cityKey}?apikey=${apiKey}&language=pt-br`;
 
 
 const fetchData = async url => {
@@ -27,11 +27,4 @@ const fetchData = async url => {
 }
 
 const getCityData = cityName => fetchData(getCityUrl(cityName))
-
-const getCityWeather = async cityName => {
-    const [cityData] = await getCityData(cityName);
-    return fetchData(getWeatherUrl(cityData));
-} 
-
-getCityWeather('São Paulo')
-    .then(console.log)
+const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey));
